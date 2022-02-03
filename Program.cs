ea897157ca9d4127cee7da3d8f246605fd74db72
Shobha1138 <yamuna.eswar@gmail.com>
2022-02-03 20:11:15 +0100

    New Changes

--- Program.cs
+++ Program.cs
@@ -15,17 +15,19 @@ namespace ConsoleApp_MP2_L3
 
             while (true)
             {
+
+
                 Console.Write("Enter type, (C)omputer or (P)hone or (R)ead or (U)pdate or (D)elete: ");
                 ConsoleKeyInfo choice = Console.ReadKey();
                 Console.WriteLine();
 
+
                 if (choice.Key == ConsoleKey.C || choice.Key == ConsoleKey.P)
                 {
-                   
-                    List<ITAsset3> sorteddt = assets.OrderBy(data => data.PurchaseDate).ToList();
 
-                    Console.WriteLine("Office Code: ");
-                    string Office = Console.ReadLine();
+
+                    List<ITAsset3> sorteddt = db1.ITAssets3.OrderBy(data => data.Office).ThenBy(data => data.PurchaseDate).ToList();
+                    Console.WriteLine("Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
 
                     foreach (ITAsset3 data in sorteddt)
                     {
@@ -42,28 +44,28 @@ namespace ConsoleApp_MP2_L3
                         {
 
                             Console.ForegroundColor = ConsoleColor.Red;
-                            if (Office == "SE")
+                            if (data.Office == "SE")
                             {
 
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"SEK{(data.Price) * 8}".ToString().PadRight(10));
                                 Console.ResetColor();
- 
+
                             }
-                            else if (Office == "FR")
+                            else if (data.Office == "FR")
                             {
 
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"EUR{(data.Price) * 1.5}".ToString().PadRight(10));
                                 Console.ResetColor();
- 
+
                             }
                             else
                             {
 
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"USD{data.Price}".ToString().PadRight(10));
                                 Console.ResetColor();
- 
+
                             }
-                            
+
                         }
 
                         if (itemDays1 > alarmDays2 && itemDays1 < alarmDays1)
@@ -71,65 +73,65 @@ namespace ConsoleApp_MP2_L3
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
 
-                            if (Office == "SE")
+                            if (data.Office == "SE")
                             {
 
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"SEK{(data.Price) * 8}".ToString().PadRight(10));
                                 Console.ResetColor();
-  
+
                             }
-                            else if (Office == "FR")
+                            else if (data.Office == "FR")
                             {
                                 data.Price = data.Price * 1.5;
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"EUR{(data.Price) * 1.5}".ToString().PadRight(10));
                                 Console.ResetColor();
- 
+
                             }
                             else
                             {
 
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"USD{data.Price}".ToString().PadRight(10));
                                 Console.ResetColor();
-                               
+
                             }
-                            Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
+
 
                         }
 
                         if (itemDays1 < alarmDays2)
                         {
-                            if (Office == "SE")
+                            if (data.Office == "SE")
                             {
-        
-                                
+
+
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"SEK{(data.Price) * 8}".ToString().PadRight(10));
-                                
+
                             }
-                            else if (Office == "FR")
+                            else if (data.Office == "FR")
                             {
-                                
+
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"EUR{(data.Price) * 1.5}".ToString().PadRight(10));
-                                
+
                             }
                             else
                             {
 
-                                
+
                                 Console.WriteLine(data.GetType().Name.PadRight(15) + data.Office.PadRight(8) + data.Brand.PadRight(15) + data.Model.PadRight(15) + data.PurchaseDate.ToString().PadRight(28) + $"USD{data.Price}".ToString().PadRight(10));
-                               
+
                             }
-                            
+
                         }
 
 
-                        Console.WriteLine("DATA IN ASSET DATABASE: ");
-                        Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
-                        db1.ITAssets3.ToList().ForEach(assetdb => Console.WriteLine(assetdb.Id.ToString().PadRight(5) + assetdb.GetType().Name.PadRight(15) + assetdb.Office.PadRight(8) + assetdb.Brand.PadRight(15) + assetdb.Model.PadRight(15) + assetdb.PurchaseDate.ToString().PadRight(28) + $"USD{assetdb.Price}".ToString().PadRight(10)));
-                        Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
-                     
+
+
                     }
 
 
+
+                    Console.WriteLine("Enter Office(SE/FR/US): ");
+                    string Office = Console.ReadLine();
                     Console.WriteLine("Brand: ");
                     string Brand = Console.ReadLine();
                     Console.WriteLine("Model: ");
@@ -153,43 +155,48 @@ namespace ConsoleApp_MP2_L3
                     }
 
 
+
                 }
 
 
 
-                if (choice.Key == ConsoleKey.U)
-                        {
-                            UpdateInDatabase();
-                            Console.WriteLine("DATA Updated in DATABASE: ");
-                            Console.WriteLine();
-                            Console.WriteLine("DATA IN ASSET DATABASE: ");
-                            Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
-                            db1.ITAssets3.ToList().ForEach(assetdb => Console.WriteLine(assetdb.Id.ToString().PadRight(5) + assetdb.GetType().Name.PadRight(15) + assetdb.Office.PadRight(8) + assetdb.Brand.PadRight(15) + assetdb.Model.PadRight(15) + assetdb.PurchaseDate.ToString().PadRight(28) + assetdb.Price.ToString().PadRight(10)));
-                            Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
 
-                        }
+                if (choice.Key == ConsoleKey.U)
+                {
+                    UpdateInDatabase();
+                    Console.WriteLine("DATA Updated in DATABASE: ");
+                    Console.WriteLine();
+                    //Console.WriteLine("DATA IN ASSET DATABASE: ");
+                    //Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
+                    //db1.ITAssets3.ToList().ForEach(assetdb => Console.WriteLine(assetdb.Id.ToString().PadRight(5) + assetdb.GetType().Name.PadRight(15) + assetdb.Office.PadRight(8) + assetdb.Brand.PadRight(15) + assetdb.Model.PadRight(15) + assetdb.PurchaseDate.ToString().PadRight(28) + assetdb.Price.ToString().PadRight(10)));
+                    //Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
+                    ReadFromDatabase();
+                    Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
+                }
 
 
 
                 if (choice.Key == ConsoleKey.D)
-                        {
-                            DeleteFromDatabase();
-                            Console.WriteLine("DATA Daleted in DATABASE: ");
-                            Console.WriteLine();
-                            Console.WriteLine("DATA IN ASSET DATABASE: ");
-                            Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
-                            db1.ITAssets3.ToList().ForEach(assetdb => Console.WriteLine(assetdb.Id.ToString().PadRight(5) + assetdb.GetType().Name.PadRight(15) + assetdb.Office.PadRight(8) + assetdb.Brand.PadRight(15) + assetdb.Model.PadRight(15) + assetdb.PurchaseDate.ToString().PadRight(28) + assetdb.Price.ToString().PadRight(10)));
-                            Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
+                {
+                    DeleteFromDatabase();
+                    Console.WriteLine("DATA Daleted in DATABASE: ");
+                    Console.WriteLine();
+                    Console.WriteLine("DATA IN ASSET DATABASE: ");
+                    Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Office".PadRight(8) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Price".PadRight(10));
+                    db1.ITAssets3.ToList().ForEach(assetdb => Console.WriteLine(assetdb.Id.ToString().PadRight(5) + assetdb.GetType().Name.PadRight(15) + assetdb.Office.PadRight(8) + assetdb.Brand.PadRight(15) + assetdb.Model.PadRight(15) + assetdb.PurchaseDate.ToString().PadRight(28) + assetdb.Price.ToString().PadRight(10)));
+                    Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
 
-                        }
+                }
 
 
                 if (choice.Key == ConsoleKey.R)
-                        {
-                            ReadFromDatabase();
+                {
+                    ReadFromDatabase();
+                    Console.WriteLine("Number of Assets: " + db1.ITAssets3.Count());
+
+                }
 
 
-                        }
 
 
             }
@@ -200,7 +207,7 @@ namespace ConsoleApp_MP2_L3
         private static void ReadFromDatabase()
         {
             Console.WriteLine("Data in ITASSET database");
-            Console.WriteLine("Id" + "    " + "NAME" + "    " + "Brand" + "       " + "Model" + "     " + "PurchaseDate" + "                          " + "OFFICE" + "    " +"PRICE");
+            Console.WriteLine("Id".PadRight(5) + "Type".PadRight(15) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchasedate".PadRight(28) + "Office".PadRight(15) + "Price".PadRight(10));
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ITAsset3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -217,10 +224,12 @@ namespace ConsoleApp_MP2_L3
                         string model = reader.GetString(2);
                         DateTime purchasedate = reader.GetDateTime(3);
                         double price = reader.GetDouble(4);
-                        string name = reader.GetString(6);
+                        string type = reader.GetString(6);
                         string office = reader.GetString(5);
 
-                        Console.WriteLine(id + "      " + name+ "      " + brand + "       " + model + "        " + purchasedate + "                   " + office + "      "+ price);
+                        Console.WriteLine(id.ToString().PadRight(5) + type.PadRight(15) + brand.PadRight(15) + model.PadRight(15) + purchasedate.ToString().PadRight(28) + office.PadRight(15) + price);
+
+
                     }
 
 
@@ -229,31 +238,32 @@ namespace ConsoleApp_MP2_L3
         }
         private static void UpdateInDatabase()
         {
+
+            int updatedRows;
+            Console.WriteLine("Update data in Asset database");
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ITAsset3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                int updatedRows;
-                Console.WriteLine("Update data in Asset database");
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ITAsset3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                connection.Open();
+                using (SqlCommand command = new
+                    SqlCommand("UPDATE ITAssets3 SET Brand = @brand WHERE Id = @id", connection))
+
                 {
-                    connection.Open();
-                    using (SqlCommand command = new
-                        SqlCommand("UPDATE ITAssets3 SET Brand = @brand WHERE Id = @id", connection))
+                    Console.WriteLine("SELECT ASSET TO UPDATE: ");
+                    int value = int.Parse(Console.ReadLine());
+                    command.Parameters.AddWithValue("@id", value);
+                    Console.WriteLine("ENTER ASSET NEW BRAND NAME: ");
+                    string newbrand = Console.ReadLine();
+                    command.Parameters.AddWithValue("@brand", newbrand);
+                    updatedRows = command.ExecuteNonQuery();
 
-                    {
-                        Console.WriteLine("SELECT ASSET TO UPDATE: ");
-                        int value = int.Parse(Console.ReadLine());
-                        command.Parameters.AddWithValue("@id", value);
-                        Console.WriteLine("ENTER ASSET NEW BRAND NAME: ");
-                        string newbrand = Console.ReadLine();
-                        command.Parameters.AddWithValue("@brand", newbrand);
-                        updatedRows = command.ExecuteNonQuery();
-
-                    }
 
                 }
 
-
             }
+
+
+
         }
 
         private static void DeleteFromDatabase()
@@ -281,7 +291,7 @@ namespace ConsoleApp_MP2_L3
     {
 
 
-        public ITAsset3(string brand, string model, DateTime purchaseDate, double price,string office)
+        public ITAsset3(string brand, string model, DateTime purchaseDate, double price, string office)
         {
             Brand = brand;
             Model = model;
@@ -296,7 +306,7 @@ namespace ConsoleApp_MP2_L3
         public string Model { get; set; }
         public DateTime PurchaseDate { get; set; }
         public double Price { get; set; }
-        public string Office { get; set; }  
+        public string Office { get; set; }
 
     }
 
@@ -308,7 +318,7 @@ namespace ConsoleApp_MP2_L3
     }
     class Phone : ITAsset3
     {
-        public Phone(string brand, string model, DateTime purchaseDate, double price,string office) : base(brand, model, purchaseDate, price, office)
+        public Phone(string brand, string model, DateTime purchaseDate, double price, string office) : base(brand, model, purchaseDate, price, office)
         {
         }
     }
